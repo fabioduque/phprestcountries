4f549285de472b285e665230f5f3a414d5fe81f7
Fábio Duque <fabio.duque.silva@gmail.com>
2015-09-04 10:24:16 +0100

    Some minor comments fixing; Changing .gitignore; Removing cached files already ignored

--- src/controllers/Countries.php
+++ src/controllers/Countries.php
@@ -19,27 +19,25 @@ class Countries
 
         // Apply the necessary filters
 
-        // *********************************************************************** 
+        // ********************************************************************
         //
-        // Explanation about count and pages, based on a total item count of 123:
+        // Explanation about count and pages:
         //  
         //  array_slice ($array, $offset, $length)
         //
-        //  # $count==0 and $page==1
-        //    $offset == $count * ($page-1) == 0 * 0 == 0  
-        //    $length == NULL, since $count is 0 == take everything until the end
+        //  # With $count==0 and $page==1
+        //    $offset == $count * ( $page - 1 ) == 0 * 0 == 0  
+        //    $length == NULL, since $count is 0 (equivalent to take all)
         // 
-        //  # $count==10 and $page==2 (takes 10, skips 10)
-        //    $offset == $count * $page-1) == 10 * (2-1) == 10 (skips 10)
+        //  # With $count==10 and $page==2 (takes 10, skips 10)
+        //    $offset == $count * ( $page - 1 ) == 10 * (2-1) == 10 (skips 10)
         //    $length == $count == 10 (takes 10)
         //    
         //
-        // *********************************************************************** 
+        // ********************************************************************
 
         $offset = $count * ( $page - 1 );
         $length = ($count == 0) ? NULL : $count;
-
-                // echo "<hr>offset = ".$offset." length = ".$length;
 
         return array_slice($allCountries, $offset, $length);
 
